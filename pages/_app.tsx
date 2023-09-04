edc7e40c031dafde39adc00d406ee887537ab322
xywei <wxy0516@gmail.com>
2023-09-04 08:29:16 -0500

    Clean up

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import 'nextra-theme-blog/style.css'
+import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import '../styles/main.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+      </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
+}
