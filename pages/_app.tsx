36cfd6aae99d2eae695bd1bc4abcdfe6ecbcda61
xywei <wxy0516@gmail.com>
2023-09-04 08:54:59 -0500

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import 'nextra-theme-blog/style.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
+import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className={inter.className}>
         <Component {...pageProps} />
       </main>
+      <Analytics />
     </>
   )
 }
