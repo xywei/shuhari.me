0b74c1f3a0a3e0a8fa341a449e78ce3c641252ee
xywei <wxy0516@gmail.com>
2024-05-03 10:53:33 -0500

    Add speed insight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   const meta = {
@@ -25,6 +26,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   )
